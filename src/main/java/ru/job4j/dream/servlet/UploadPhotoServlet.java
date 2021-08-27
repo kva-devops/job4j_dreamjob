7bf9bf82d6f0336005812d4591079e08c1123254
vladimirKutyavin <kva.devops@gmail.com>
2021-08-27 11:29:28 +0400

    1. Загрузка и скачивание файла. [#154183 #195338] - correcting

--- src/main/java/ru/job4j/dream/servlet/UploadPhotoServlet.java
+++ src/main/java/ru/job4j/dream/servlet/UploadPhotoServlet.java
@@ -5,7 +5,6 @@ import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,21 +13,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UploadPhotoServlet extends HttpServlet {
-//    @Override
-//    protected void doGet(HttpServletRequest req,
-//                         HttpServletResponse resp) throws ServletException, IOException {
-////        List<String> images = new ArrayList<>();
-////        for (File name : new File("../images/").listFiles()) {
-////            images.add(name.getName());
-////        }
-////        req.setAttribute("images", images);
-//        RequestDispatcher dispatcher = req.getRequestDispatcher("/candidates.jsp");
-//        dispatcher.forward(req, resp);
-//    }
 
     @Override
     protected void doPost(HttpServletRequest req,
