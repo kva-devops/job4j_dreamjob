6961a11b2b6a9722bb0a5622d7bce39d11b874ff
vladimirKutyavin <kva.devops@gmail.com>
2021-08-28 10:04:56 +0400

    0. Страница Login.jsp [#281230]

---
+++ src/main/java/ru/job4j/dream/servlet/AuthServlet.java
@@ -0,0 +1,22 @@
+package ru.job4j.dream.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req,
+                          HttpServletResponse resp) throws IOException, ServletException {
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        if ("root@local".equals(email) && "root".equals(password)) {
+            resp.sendRedirect(req.getContextPath() + "/posts.do");
+        } else {
+            req.setAttribute("error", "Wrong login or password. Try again.");
+            req.getRequestDispatcher("login.jsp").forward(req, resp);
+        }
+    }
+}
